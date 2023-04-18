0d847e2c332861b71c60cd288bcccd105eb59b6c
Daniel Blanco Parla <dblancoparla@gmail.com>
2023-04-18 19:06:50 +0200

    chore: build target

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,8 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-    define: {
-        global: {},
-    },
     plugins: [dts({ insertTypeEntry: true })],
     build: {
-        target: 'es2020',
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
             name: 'p2p',
