121cb858b73dbdc2b7102d06a474fb462afe337c
Daniel Blanco Parla <dblancoparla@gmail.com>
2023-04-25 11:49:47 +0200

    feat: update node for using WS and WebRTC

--- src/node.ts
+++ src/node.ts
@@ -1,5 +1,6 @@
 import * as LibP2P from 'libp2p'
-import { tcp } from '@libp2p/tcp'
+import { webSockets } from '@libp2p/websockets'
+import { webRTCStar } from '@libp2p/webrtc-star'
 import { mplex } from '@libp2p/mplex'
 import { noise } from '@chainsafe/libp2p-noise'
 import { bootstrap } from '@libp2p/bootstrap'
@@ -7,15 +8,17 @@ import { kadDHT } from '@libp2p/kad-dht'
 import { gossipsub } from '@chainsafe/libp2p-gossipsub'
 import { pubsubPeerDiscovery } from '@libp2p/pubsub-peer-discovery'
 
-export const defaultOptions = {
-  transports: [tcp()],
+const topics = ['__stability__._peer-discovery._p2p._pubsub']
+
+export const defaultOptions: LibP2P.Libp2pOptions = {
+  transports: [webSockets(), webRTCStar().transport],
   streamMuxers: [mplex()],
   connectionEncryption: [noise()],
-  peerDiscovery: [pubsubPeerDiscovery()],
+  peerDiscovery: [pubsubPeerDiscovery({ topics })],
   dht: kadDHT(),
   pubsub: gossipsub({ allowPublishToZeroPeers: true }),
   addresses: {
-    listen: ['/ip4/0.0.0.0/tcp/0']
+    listen: ['/ip4/0.0.0.0/tcp/0/ws']
   }
 }
 
