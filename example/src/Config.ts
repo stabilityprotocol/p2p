023cc11a1953b5bba8330b26014a24c995ad9a62
Daniel Blanco Parla <dblancoparla@gmail.com>
2023-05-01 23:31:05 +0200

    feat: update WSS conn

--- example/src/Config.ts
+++ example/src/Config.ts
@@ -1,5 +1,5 @@
 export const Config = {
   chatTopic: 'chat.__stability__',
   relayAddr:
-    '/ip4/206.189.101.167/tcp/46265/ws/p2p/12D3KooWM2hNu14DdPErEo4G1cbpMj5fcxLZX9e4jezJXHgr7nhX'
+    '/dns4/wss.unifiprotocol.com/tcp/443/wss/p2p/12D3KooWM2hNu14DdPErEo4G1cbpMj5fcxLZX9e4jezJXHgr7nhX'
 }
